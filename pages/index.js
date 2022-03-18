62bdcc7673e8de5f522add0c88dadf1869570202
afroz23 <afroziiitian@gmail.com>
2022-03-18 17:38:27 +0530

    folder structure modified

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
 import React from "react";
 import Head from "next/head";
-import About from "./about/index";
-import Projects from "./projects/index";
-import Experience from "./Experience/index";
-import Contact from "./contact/index";
-import Homepage from "../Components/Homepage";
-import Navbar from "../Components/Navbar";
-import Footer from "./Footer/index";
+import About from "./about";
+import Projects from "./projects";
+import Experience from "./experience";
+import Contact from "./contact";
+import Homepage from "../src/Components/Homepage";
+import Navbar from "../src/Components/Navbar";
+import Footer from "./footer";
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>Afroz Quraishi</title>
-                <meta
-                    name="description"
-                    content="My name is Afroz Quraishi. I am a pre-final year student in the department of Information Technology @IIIT Gwalior. Currently, I am working in a startup as a Frontend Developer, Next.js and I have been working with Reactjs and related libraries/frameworks since 2020."
-                />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>Afroz Quraishi</title>
+        <meta
+          name="description"
+          content="My name is Afroz Quraishi. I am a pre-final year student in the department of Information Technology @IIIT Gwalior. Currently, I am working in a startup as a Frontend Developer, Next.js and I have been working with Reactjs and related libraries/frameworks since 2020."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-            <Navbar />
-            <Homepage />
-            <hr />
-            <About />
-            <hr />
-            <Experience />
-            <hr />
-            <Projects />
-            <hr />
-            <Contact />
-            <Footer />
-        </div>
-    );
+      <Navbar />
+      <Homepage />
+      <hr />
+      <About />
+      <hr />
+      <Experience />
+      <hr />
+      <Projects />
+      <hr />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/Components/projectDetails.js
@@ -0,0 +1,50 @@
+export const projectDetails = [
+  // {
+  //     projectLink: "",
+  //     deployLink: "",
+  //     projectTitle: "",
+  //     projectDetail: "",
+  // },
+  {
+    projectLink: "https://github.com/afroz23/afrozquraishi",
+    deployLink: "https://afrozquraishi.vercel.app/",
+    projectTitle: "Portfolio",
+    projectDetail:
+      "This portfolio is created with Next.js. I used pure CSS to make it responsive NO Bootstrap is used in this Portfolio.",
+  },
+  {
+    projectLink: "https://github.com/afroz23/Meme_generator",
+    deployLink: "https://afroz23.github.io/Meme_generator/",
+    projectTitle: "MEME GENERATOR",
+    projectDetail:
+      "Developed a meme generator application using Imgflip API. Fetches all the Images and make a post request with two text/sentence and generates a Meme of the chosen Image.",
+  },
+
+  {
+    projectLink: "https://github.com/afroz23/linked-list",
+    deployLink: "",
+    projectTitle: "Linked List",
+    projectDetail:
+      "Tried to code all the functionality of linked list in c++ language.",
+  },
+  {
+    projectLink: "https://github.com/afroz23/Todo-App",
+    deployLink: "https://afroz23.github.io/Todo-App/",
+    projectTitle: "Todo App",
+    projectDetail:
+      "An advance Todo app built in ReactJS. You can Add, cross-off(complete), delete todo from the lists. You can also list all todos, completed todos, toggle all todos to Complete/ Incomplete, and you can also delete all cross-off todos.",
+  },
+  {
+    projectLink: "https://github.com/afroz23/chat-room-app",
+    deployLink: "https://chat-room-application.netlify.app/",
+    projectTitle: "Chat Room App",
+    projectDetail:
+      "It is a chatroom application for group chatting. Admin will provide you username and password. Admin can hide their credential So that users can talk freely.",
+  },
+  {
+    projectLink: "https://github.com/afroz23/Guess-The-Number",
+    projectTitle: "Guess The Number",
+    projectDetail:
+      "It is a python based programme where the computer randomly generates a number between 0 and 20. The user needs to guess the number.",
+  },
+];
