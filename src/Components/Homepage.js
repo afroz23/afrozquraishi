974e4d089f2a83a1a95d292c7fd652bf602c4df5
afroz23 <afroziiitian@gmail.com>
2022-03-18 18:38:19 +0530

    animation removed

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,7 +5,7 @@ import Typed from "react-typed";
 import { FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
 import profile from "../../public/Images/profile.svg";
-import { useSpring, animated } from "react-spring";
+import { Spring } from "react-spring";
 import {
   Link,
   Button,
@@ -16,101 +16,94 @@ import {
   scroller,
 } from "react-scroll";
 function Home() {
-  const props = useSpring({
-    from: { rotateZ: 180 },
-    to: { rotateZ: 0 },
-    delay: 600,
-  });
   return (
-    <animated.div style={props}>
-      <Element name="home">
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.col_6}>
-              <div className={styles.header__content}>
-                <div className={styles.header__section}>
-                  <div className={styles.header_font_size}>
-                    <div>
-                      Hi, I'm <span className={styles.span_afroz}>Afroz</span>{" "}
-                      Quraishi
-                    </div>
+    <Element name="home">
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.col_6}>
+            <div className={styles.header__content}>
+              <div className={styles.header__section}>
+                <div className={styles.header_font_size}>
+                  <div>
+                    Hi, I'm <span className={styles.span_afroz}>Afroz</span>{" "}
+                    Quraishi
                   </div>
-                  <Typed
-                    className={styles.typed_text}
-                    strings={[
-                      "Competitive Programmer",
-                      "Full Stack Developer",
-                      "Open Source Contributor",
-                      "Problem Solver",
-                    ]}
-                    typeSpeed={80}
-                    backSpeed={10}
-                    loop
-                  ></Typed>
+                </div>
+                <Typed
+                  className={styles.typed_text}
+                  strings={[
+                    "Competitive Programmer",
+                    "Full Stack Developer",
+                    "Open Source Contributor",
+                    "Problem Solver",
+                  ]}
+                  typeSpeed={80}
+                  backSpeed={10}
+                  loop
+                ></Typed>
 
-                  <div className={styles.header__buttons}>
-                    <Link
-                      activeClass="active"
-                      spy={true}
-                      smooth={true}
-                      offset={-55}
-                      duration={500}
-                      to="about"
-                      className={styles.more_about}
-                    >
-                      More About me
-                    </Link>
-                  </div>
-                  <div className={styles.header__ul}>
-                    <a
-                      className={styles.header_anchor}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://github.com/afroz23"
-                    >
-                      <AiFillGithub className={styles.headerIcon} />
-                    </a>
-                    <a
-                      className={styles.header_anchor}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.linkedin.com/in/afrozquraishi/"
-                    >
-                      <FaLinkedinIn className={styles.headerIcon} />
-                    </a>
-                    <a
-                      className={styles.header_anchor}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://twitter.com/AfrozQuraishi_"
-                    >
-                      <FaTwitter className={styles.headerIcon} />
-                    </a>
-                    <a
-                      className={styles.header_anchor}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://www.instagram.com/afrozquraishi_/"
-                    >
-                      <FaInstagram className={styles.headerIcon} />
-                    </a>
-                  </div>
+                <div className={styles.header__buttons}>
+                  <Link
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-55}
+                    duration={500}
+                    to="about"
+                    className={styles.more_about}
+                  >
+                    More About me
+                  </Link>
+                </div>
+                <div className={styles.header__ul}>
+                  <a
+                    className={styles.header_anchor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/afroz23"
+                  >
+                    <AiFillGithub className={styles.headerIcon} />
+                  </a>
+                  <a
+                    className={styles.header_anchor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/afrozquraishi/"
+                  >
+                    <FaLinkedinIn className={styles.headerIcon} />
+                  </a>
+                  <a
+                    className={styles.header_anchor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://twitter.com/AfrozQuraishi_"
+                  >
+                    <FaTwitter className={styles.headerIcon} />
+                  </a>
+                  <a
+                    className={styles.header_anchor}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.instagram.com/afrozquraishi_/"
+                  >
+                    <FaInstagram className={styles.headerIcon} />
+                  </a>
                 </div>
               </div>
             </div>
-            <div className={styles.col_6}>
-              <div className={styles.banner__img}>
-                <Image
-                  className={styles.profile}
-                  src={profile}
-                  alt="profile pic"
-                />
-              </div>
+          </div>
+          <div className={styles.col_6}>
+            <div className={styles.banner__img}>
+              <Image
+                className={styles.profile}
+                src={profile}
+                alt="profile pic"
+              />
             </div>
           </div>
         </div>
-      </Element>
-    </animated.div>
+      </div>
+    </Element>
   );
 }
 
